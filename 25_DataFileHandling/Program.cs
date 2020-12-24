5703f054e136cafe28d581433e27850089afe0e8
kmranrg <kmranrg@gmail.com>
2020-12-24 15:08:34 +0530

    data file handling

---
+++ 25_DataFileHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO; // for data file handling
+
+namespace _25_DataFileHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DATA FILE HANDLING\n");
+
+            // Writing to a file and then reading it
+
+            string writeText = "Hello World!";  // Create a text string
+            File.WriteAllText("myFile.txt", writeText);  // Create a file and write the content of writeText to it
+
+            writeText = "\nMy name is Kumar Anurag and my twitter id is kmranrg.";
+            File.AppendAllText("myFile.txt",writeText); // it adds the content at the end of the file
+
+            string readText = File.ReadAllText("myFile.txt");  // Read the contents of the file
+            Console.WriteLine(readText);  // Output the content
+
+            /*
+            OUTPUT:
+
+            DATA FILE HANDLING
+
+            Hello World!
+            My name is Kumar Anurag and my twitter id is kmranrg.
+            */
+
+        }
+    }
+}
