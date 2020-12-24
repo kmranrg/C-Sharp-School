31b0e54e404304b921d83226e4b93d2eca40515b
kmranrg <kmranrg@gmail.com>
2020-12-24 13:27:51 +0530

    method overloading

---
+++ 23_MethodOverloading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _23_MethodOverloading
+{
+    class Program
+    {
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("METHOD OVERLOADING\n");
+
+            /*
+            KNOWLEDGE CLOUD:
+
+            1) With method overloading, multiple methods can have the same name with different parameters
+            */
+
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+
+            /*
+            OUTPUT:
+
+            METHOD OVERLOADING
+
+            Int: 13
+            Double: 10.559999999999999
+            */
+        }
+    }
+}
